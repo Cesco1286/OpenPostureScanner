2ba8c4f3807471014133b9a0c2a79a244ce51338
valerio17 <jimbo92@tiscali.it>
2016-05-27 13:40:23 +0200

    acquisizione interattiva
    La routine che c'era prima è stata commentata

--- acquisition.m
+++ acquisition.m
@@ -4,6 +4,7 @@ function [ metaDataDepth, imgColor ] = acquisition()
 
 %% setup variabili
 angleOK=0;
+acquired=0;
 vidRGB = videoinput('kinect',1,'RGB_640x480');
 %srcRGB = getselectedsource(vidRGB);
 
@@ -15,11 +16,11 @@ preview(vidRGB);
 while angleOK==0,
     
     angle=input('inserire l''angolo da aggiungere all''attuale\n');
-	if srcDepth.CameraElevationAngle+angle>=0 & srcDepth.CameraElevationAngle+angle<=27
-    srcDepth.CameraElevationAngle=srcDepth.CameraElevationAngle+angle;
-	end
-    angleOK=input('se l''angolo è ok inserire 1, 0 altrimenti');
-   
+    if srcDepth.CameraElevationAngle+angle>=0 & srcDepth.CameraElevationAngle+angle<=27
+        srcDepth.CameraElevationAngle=srcDepth.CameraElevationAngle+angle;
+    end
+    angleOK=input('se l''angolo è ok inserire 1, 0 altrimenti\n');
+    
 end
 closepreview(vidRGB);
 
@@ -34,31 +35,52 @@ vidDepth.TriggerRepeat=200;
 vidRGB.TriggerRepeat=200;
 
 triggerconfig([vidDepth vidRGB],'manual');
+
 %% start acquisizione
 start([vidDepth vidRGB]);
 
-% Get the data on the object.
-trigger([vidDepth vidRGB]); %cattura i frame
-
-[frame, ts, metaDataDepth] = getdata(vidDepth);
-[imgColor, ts_rgb, metaDataRGB] = getdata(vidRGB);
-
-
-imshow(imgColor(:,:,1:3),[]);
-answer= input('Is this image ok? y/n ');
-if answer = y || metaDataDepth.IsSkeletonTracked==0,
-    while answer==n || metaDataDepth.IsSkeletonTracked==0,
-        if metaDataDepth.IsSkeletonTracked==0,
-            disp('lo scheletro non è stato riconosciuto.');
-        end
-        trigger([vidRGB vidDepth]); 
+% controllo interattivo sull'acquisizione, continua ad acquisire fino a che
+% si inserisce 1 alla domanda dell'acquisizione corretta
+%
+while acquired==0,
+    if input('premere 1 per fare una nuova acquisizione\n'),
+        trigger([vidDepth vidRGB]);
         [frame, ts, metaDataDepth] = getdata(vidDepth);
         [imgColor, ts_rgb, metaDataRGB] = getdata(vidRGB);
-        imshow(imgColor(:,:,1:3),[]);
-        answer= input('Is this image ok? y/n ');
-    end 	
-end			
+        
+        %attualmente la chiamata dello skeletonViewer funziona ma non
+        %riesco a tirare fuori l'ID dello scheletro. Sicuramente
+        %qualcun'altro l'ha fatto prima di noi.
+        % skID=metaDataDepth.JointImageIndices(:,:,~=0);
+        skeletonViewer(metaDataDepth.JointImageIndices,imgColor,1,ERROR);
+        acquired=input('inserire 1 se l''acquisizione è corretta, 0 altrimenti\n');
+    end
+end
 
 stop([vidDepth vidRGB]);
 return;
+
+
+
+% controllo su "metaData.isSkeletonTracked" -> se è 1, possiamo fermare l'acquisizione,altrimenti dobbiamo riottenere i metadati.(sia DEPTH che RGB)
+%
+%skelViewer
+% imshow(imgColor(:,:,1:3),[]);
+% answer=input('Is this image ok? y/n ','s');
+% if answer == 'y' || metaDataDepth.IsSkeletonTracked==0,
+%     while answer=='n' || metaDataDepth.IsSkeletonTracked==0,
+%         if metaDataDepth.IsSkeletonTracked==0,
+%             disp('lo scheletro non è stato riconosciuto.');
+%         end
+%         trigger([vidRGB vidDepth]);
+%         [frame, ts, metaDataDepth] = getdata(vidDepth);
+%         [imgColor, ts_rgb, metaDataRGB] = getdata(vidRGB);
+%
+%         imshow(imgColor(:,:,1:3),[]);
+%         answer= input('Is this image ok? y/n ','s');
+%     end
+% end
+%
+% stop([vidDepth vidRGB]);
+% return;
 end
