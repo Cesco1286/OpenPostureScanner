01b1afda61ec9ae25937d98348fb91d564196444
valerio17 <jimbo92@tiscali.it>
2016-06-04 16:28:47 +0200

    empty funs

---
+++ acquisitionBalance.m
@@ -0,0 +1,8 @@
+function [info] = acquisitionBalance()
+%routine interattiva di acquisizione distribuzione del peso, l'output è
+%parserabile da Visualizer.m
+%   Detailed explanation goes here
+
+
+end
+
---
+++ skeletonAnalizer.m
@@ -0,0 +1,8 @@
+function [ errori ] = skeletonAnalizer( skeleton )
+%analisi della postura con riferimenti interni, l'output è parserabile da
+%Visualizer.m
+%   Detailed explanation goes here
+
+
+end
+
