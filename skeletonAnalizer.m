285e7e3d2946f35a6a7c3fdef64dd18d72fadbd3
valerio17 <jimbo92@tiscali.it>
2016-07-12 12:47:31 +0200

    Delete SkeletonAnalizer.m

--- skeletonAnalizer.m
+++
@@ -1,8 +0,0 @@
-function [ errori ] = skeletonAnalizer( skeleton )
-%analisi della postura con riferimenti interni, l'output è parserabile da
-%Visualizer.m
-%   Detailed explanation goes here
-
-
-end
-
