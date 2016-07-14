21b750d2a7d2860c4467cf66b97256983b081ac3
huly94 <ico.giulianelli@hotmail.it>
2016-07-14 10:46:08 +0200

    Update ImpostaParametri.m

--- ImpostaParametri.m
+++ ImpostaParametri.m
@@ -22,7 +22,7 @@ function varargout = ImpostaParametri(varargin)
 
 % Edit the above text to modify the response to help ImpostaParametri
 
-% Last Modified by GUIDE v2.5 12-Jul-2016 12:30:58
+% Last Modified by GUIDE v2.5 13-Jul-2016 22:50:13
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -75,8 +75,8 @@ varargout{1} = handles.output;
 
 
 % --- Executes during object creation, after setting all properties.
-function density_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to density (see GCBO)
+function SchienaSp_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to SchienaSp (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
@@ -88,21 +88,21 @@ end
 
 
 
-function density_Callback(hObject, eventdata, handles)
-% hObject    handle to density (see GCBO)
+function SchienaSp_Callback(hObject, eventdata, handles)
+% hObject    handle to SchienaSp (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hints: get(hObject,'String') returns contents of density as text
-%        str2double(get(hObject,'String')) returns contents of density as a double
-density = str2double(get(hObject, 'String'));
-if isnan(density)
+% Hints: get(hObject,'String') returns contents of SchienaSp as text
+%        str2double(get(hObject,'String')) returns contents of SchienaSp as a double
+SchiSpa = str2double(get(hObject, 'String'));
+if isnan(SchiSpa)
     set(hObject, 'String', 0);
     errordlg('Input must be a number','Error');
 end
 
-% Save the new density value
-handles.metricdata.density = density;
+% Save the new SchienaSp value
+handles.metricdata.SchiSpa = SchiSpa;
 guidata(hObject,handles)
 
 % --- Executes during object creation, after setting all properties.
@@ -178,18 +178,163 @@ if isfield(handles, 'metricdata') && ~isreset
     return;
 end
 
-handles.metricdata.density = 0;
-handles.metricdata.volume  = 0;
+handles.metricdata.SchiSpa = 0;
+handles.metricdata.Schi = 0;
+handles.metricdata.BacSchi = 0;
+handles.metricdata.DiffFia = 0;
+handles.metricdata.DiffGin = 0;
 
-set(handles.density, 'String', handles.metricdata.density);
-set(handles.volume,  'String', handles.metricdata.volume);
-set(handles.mass, 'String', 0);
+set(handles.SchienaSp, 'String', handles.metricdata.SchiSpa);
+set(handles.SchienaText, 'String', handles.metricdata.Schi);
+set(handles.BacSchText, 'String', handles.metricdata.BacSchi);
+set(handles.JointFianchiText, 'String', handles.metricdata.DiffFia);
+set(handles.JointGinocchiaText, 'String', handles.metricdata.DiffGin);
 
-set(handles.unitgroup, 'SelectedObject', handles.english);
 
-set(handles.text4, 'String', 'lb/cu.in');
-set(handles.text5, 'String', 'cu.in');
-set(handles.text6, 'String', 'lb');
+
+
+
 
 % Update handles structure
 guidata(handles.figure1, handles);
+
+
+
+function SchienaText_Callback(hObject, eventdata, handles)
+% hObject    handle to SchienaText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of SchienaText as text
+%        str2double(get(hObject,'String')) returns contents of SchienaText as a double
+Schi = str2double(get(hObject, 'String'));
+if isnan(Schi)
+    set(hObject, 'String', 0);
+    errordlg('Input must be a number','Error');
+end
+
+% Save the new Schi value
+handles.metricdata.Schi = Schi;
+guidata(hObject,handles)
+
+% --- Executes during object creation, after setting all properties.
+function SchienaText_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to SchienaText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function BacSchText_Callback(hObject, eventdata, handles)
+% hObject    handle to BacSchText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of BacSchText as text
+%        str2double(get(hObject,'String')) returns contents of BacSchText as a double
+BacSchi = str2double(get(hObject, 'String'));
+if isnan(BacSchi)
+    set(hObject, 'String', 0);
+    errordlg('Input must be a number','Error');
+end
+
+% Save the new BacSchi value
+handles.metricdata.BacSchi = BacSchi;
+guidata(hObject,handles)
+
+% --- Executes during object creation, after setting all properties.
+function BacSchText_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to BacSchText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function JointFianchiText_Callback(hObject, eventdata, handles)
+% hObject    handle to JointFianchiText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of JointFianchiText as text
+%        str2double(get(hObject,'String')) returns contents of JointFianchiText as a double
+DiffFia = str2double(get(hObject, 'String'));
+if isnan(DiffFia)
+    set(hObject, 'String', 0);
+    errordlg('Input must be a number','Error');
+end
+
+% Save the new diffFia value
+handles.metricdata.DiffFia = DiffFia;
+guidata(hObject,handles)
+
+
+% --- Executes during object creation, after setting all properties.
+function JointFianchiText_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to JointFianchiText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function JointGinocchiaText_Callback(hObject, eventdata, handles)
+% hObject    handle to JointGinocchiaText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of JointGinocchiaText as text
+%        str2double(get(hObject,'String')) returns contents of JointGinocchiaText as a double
+DiffGin = str2double(get(hObject, 'String'));
+if isnan(DiffGin)
+    set(hObject, 'String', 0);
+    errordlg('Input must be a number','Error');
+end
+
+% Save the new DiffGin value
+handles.metricdata.DiffGin = DiffGin;
+guidata(hObject,handles)
+
+% --- Executes during object creation, after setting all properties.
+function JointGinocchiaText_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to JointGinocchiaText (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in pushbuttonConfirm.
+function pushbuttonConfirm_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbuttonConfirm (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+parametri(1) = handles.metricdata.SchiSpa;
+parametri(2) = handles.metricdata.Schi;
+parametri(3) = handles.metricdata.BacSchi;
+parametri(4) = handles.metricdata.DiffFia;
+parametri(5)  = handles.metricdata.DiffGin;
+params = ParametriUtente(parametri);
+controller = Controller.getInstance();
+controller.setParametriUtente(params);
+
