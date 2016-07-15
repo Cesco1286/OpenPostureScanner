6d2da2b2431a86e9ccfaaca19ab0f8c2ba3b6583
Cesco1286 <bellapecesco@gmail.com>
2016-07-15 18:10:47 +0200

    Update Avviamento.m

--- Avviamento.m
+++ Avviamento.m
@@ -1,20 +1,3 @@
-classdef Avviamento 
-    
-    properties
-        controller;
-        uiPath;
-    end
-    
-    methods
-        function avviamento = Avviamento()
-            avviamento.controller = Controller();
-            
-        end
-        
-        function startApplication(avviamento)
-            %Ui.m 
-        end 
-    end
-    
-
-end
+clear();
+controller=Controller();
+SchermataPrincipale;
